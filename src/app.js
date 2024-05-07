e3d65731de26481f547aa727e7c7b7cb33941120
KOTHECODE <160642762+KOTHECODE@users.noreply.github.com>
2024-05-07 01:05:57 +0000

    number position

--- src/app.js
+++ src/app.js
@@ -7,4 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = () => {
   //write your code here
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+  let suit = generateRandomSuit();
+  document.querySelector(".top-suit").innerHTML = suit;
+  document.querySelector(".bottom-suit").innerHTML = suit;
+};
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+let generateRandomSuit = () => {
+  let suit = ["♠", "♥", "♦", "♣"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
 };
